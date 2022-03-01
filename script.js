e23424f258dad09d1ade2ab5d0bd4099cf1250ae
Venkatesh <76658782+venkiganesh@users.noreply.github.com>
2022-03-01 20:08:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function openmenu() {
+  let x = document.getElementById('navbartoggle');
+  x.style.animation="navopen 0.2s  forwards linear";
+
+}
+function closemenu() {
+  let x = document.getElementById('navbartoggle');
+  x.style.animation="navclose 0.1s  forwards linear";
+}
+
+function slideright() {
+  let x = document.getElementById('img-container-1');
+  let y = document.getElementById('img-container-2');
+  let z = document.getElementById('img-container-3');
+z.style.opacity="1";
+}
